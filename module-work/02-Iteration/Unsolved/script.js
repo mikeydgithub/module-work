859c64b03a91c80f2c3ebbc9a317f2bd3ed0061f
Michael Diamond <94988620+mikeydgithub@users.noreply.github.com>
2022-01-06 21:08:52 -0500

    module commit

--- module-work/02-Iteration/Unsolved/script.js
+++
@@ -1,8 +0,0 @@
-// TODO: Write Your JavaScript Code Here
-var students = ["Mike", "Jessica", "Bob", "Kyle", "Kelly"];
-
-console.log(students.length);
-
-for (var i=0; i < students.length; i++) {
-    console.log ('Great to see you, ' + students[i] + '!');
-}
